c900533b9c5a1aef31755f13c9c60eef37f06562
Abdul Rafey <syedabdulrafeyali@gmail.com>
2025-01-14 19:54:03 +0100

    Google Analytics Bug 3

--- plugins/vue-gtag.client.ts
+++ plugins/vue-gtag.client.ts
@@ -3,15 +3,23 @@ import { useRouter } from 'vue-router';
 import VueGtag from 'vue-gtag-next';
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const router = useRouter();
+    const router = useRouter();
 
-  nuxtApp.vueApp.use(VueGtag, {
-    property: {
-      id: 'G-SV1D309D78' // Replace with your Google Analytics Measurement ID
-    }
-  });
+    nuxtApp.vueApp.use(VueGtag, {
+        property: {
+            id: 'G-SV1D309D78' // Replace with your Google Analytics Measurement ID
+        }
+    });
 
-  router.afterEach((to) => {
-    nuxtApp.vueApp.$gtag.pageview(to.fullPath);
-  });
+    // Ensure page views are tracked after router navigation
+    router.afterEach((to) => {
+        if (nuxtApp.vueApp.$gtag) {
+            nuxtApp.vueApp.$gtag.pageview(to.fullPath);
+        } else {
+            console.warn('Google Analytics is not initialized yet.');
+        }
+    });
 });
+
+
+

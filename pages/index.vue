14eabe82be85fe40a544037b5757a8e74ff4d048
Abdul Rafey <syedabdulrafeyali@gmail.com>
2025-01-14 14:26:24 +0100

    Setting up Project + Header || Footer

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h2 class="text-2xl font-semibold mb-4">Welcome to My Blog</h2>
+        <p>This is the home page content!</p>
+    </div>
+</template>
+
+<script>
+export default {
+    layout: 'default',
+};
+</script>
